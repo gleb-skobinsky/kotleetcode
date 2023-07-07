aa1c653258c9befd90eb6000bfcf604ccc3e5e82
gleb-skobinsky <glebgytnik@gmail.com>
2023-07-07 17:48:49 +0300

    Reverse vowels solution in main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,6 +4,6 @@ fun main() {
     println(candies.Solution().kidsWithCandies2(listOf(12, 1, 12).toIntArray(), 4))
     println(candies.Solution().kidsWithCandies(listOf(12, 1, 12).toIntArray(), 4))
     println(flowers.Solution().canPlaceFlowers(listOf(1, 0, 0, 0, 1).toIntArray(), 2))
-    println(vowels.Solution().reverseVowels("Hello leetcode"))
-    println(vowels.Solution().reverseVowels2("Hello leetcode"))
+    println(vowels.Solution().reverseVowels("Hi leetcode"))
+    println(vowels.Solution().reverseVowels2("Hi leetcode"))
 }
